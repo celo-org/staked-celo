328c568f3f050e8f6ff7a60c2fedf6d15c5c8832
soloseng <102702451+soloseng@users.noreply.github.com>
2022-06-01 20:20:01 -0400

    ++ option to specify RPC provider

--- scripts/mineBlocks.ts
+++ scripts/mineBlocks.ts
@@ -1,19 +1,7 @@
-import hre from "hardhat";
-import { JsonRpcProvider } from "@ethersproject/providers";
-
 import { mineBlocks } from "../test-ts/utils";
 
-let devchainProvider: JsonRpcProvider;
-
-// Switch provider to mine block on ganache instead of the default hardhat fork
-function useLocalhostProvider() {
-  console.log("switching provider");
-  devchainProvider = new hre.ethers.providers.JsonRpcProvider("http://localhost:7545");
-}
-
 async function mineNBlocks(n: number) {
-  useLocalhostProvider();
-  mineBlocks(n);
+  mineBlocks(n, true);
 }
 
 mineNBlocks(35);
