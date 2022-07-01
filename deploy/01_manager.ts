606302a4d3f58a36ea6f57aeba20faffe93aad6a
Marcin Chrzanowski <martin@clabs.co>
2022-07-01 12:06:34 +0200

    Added .env option to activate validator groups at deploy time

--- deploy/01_manager.ts
+++ deploy/01_manager.ts
@@ -1,9 +1,14 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 
+const parseValidatorGroups = (validatorGroupsString: string | undefined) =>
+  validatorGroupsString ? validatorGroupsString.split(',') : []
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy } = hre.deployments;
   const { deployer } = await hre.getNamedAccounts();
+
+  const validatorGroups = parseValidatorGroups(process.env.VALIDATOR_GROUPS);
 
   const deployment = await deploy("Manager", {
     from: deployer,
@@ -18,6 +23,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       },
     },
   });
+
+  const manager = await hre.ethers.getContract('Manager');
+
+  for (let i = 0; i < validatorGroups.length; i++) {
+      console.log('activating group', validatorGroups[i]);
+      await manager.activateGroup(validatorGroups[i]);
+  }
 };
 
 func.id = "deploy_manager";
