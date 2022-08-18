f8c67cdcb71e43eef28209fe542d9b547e288f4f
soloseng <102702451+soloseng@users.noreply.github.com>
2022-08-18 17:15:48 -0400

    improved runtime

--- lib/account-tasks/helpers/withdrawalHelpter.ts
+++ lib/account-tasks/helpers/withdrawalHelpter.ts
@@ -3,6 +3,8 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { ElectionWrapper } from "@celo/contractkit/lib/wrappers/Election";
 import { BigNumber } from "ethers";
 
+const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+
 export async function withdraw(hre: HardhatRuntimeEnvironment, beneficiaryAddress: string) {
   try {
     const electionWrapper = await hre.kit.contracts.getElection();
@@ -27,68 +29,67 @@ export async function withdraw(hre: HardhatRuntimeEnvironment, beneficiaryAddres
       );
 
       if (scheduledWithdrawalAmount.gt(0)) {
+        let remainingRevokeAmount: BigNumber = BigNumber.from(0);
+        let toRevokeFromPending: BigNumber = BigNumber.from(0);
+
+        let lesserAfterPendingRevoke: string = ADDRESS_ZERO;
+        let greaterAfterPendingRevoke: string = ADDRESS_ZERO;
+        let lesserAfterActiveRevoke: string = ADDRESS_ZERO;
+        let greaterAfterActiveRevoke: string = ADDRESS_ZERO;
+
         // substract the immediateWithdrawalAmount from scheduledWithdrawalAmount to get the revokable amount
         const immediateWithdrawalAmount: BigNumber = await accountContract.scheduledVotesForGroup(
           group
         );
         console.log("DEBUG: ImmediateWithdrawalAmount:", immediateWithdrawalAmount);
 
-        let remainingRevokeAmount;
-        if (immediateWithdrawalAmount.gt(scheduledWithdrawalAmount)) {
-          remainingRevokeAmount = BigNumber.from(0);
-          console.log(`DEBUG: setting remainingRevokeAmount to: ${remainingRevokeAmount}`);
-        } else {
+        if (immediateWithdrawalAmount.lt(scheduledWithdrawalAmount)) {
           remainingRevokeAmount = scheduledWithdrawalAmount.sub(immediateWithdrawalAmount);
+
+          console.log("remainingRevokeAmount:", remainingRevokeAmount);
+
+          // get AccountContract pending votes for group.
+          const groupVote = await electionWrapper.getVotesForGroupByAccount(
+            accountContract.address,
+            group
+          );
+          const pendingVotes = groupVote.pending;
+
+          console.log("pendingVotes:", pendingVotes);
+
+          // amount to revoke from pending
+          toRevokeFromPending = BigNumber.from(
+            Math.min(remainingRevokeAmount.toNumber(), pendingVotes.toNumber())
+          );
+
+          console.log("toRevokeFromPending:", toRevokeFromPending);
+
+          // find lesser and greater for pending votes
+          const lesserAndGreaterAfterPendingRevoke =
+            await electionWrapper.findLesserAndGreaterAfterVote(
+              group,
+              // @ts-ignore: BigNumber types library conflict.
+              toRevokeFromPending.mul(-1).toString()
+            );
+          lesserAfterPendingRevoke = lesserAndGreaterAfterPendingRevoke.lesser;
+          greaterAfterPendingRevoke = lesserAndGreaterAfterPendingRevoke.greater;
+
+          // Given that validators are sorted by total votes and that revoking pending votes happen before active votes.
+          // One must acccount for any pending votes that would get removed from the total votes when revoking active votes
+          // in the same transaction.
+
+          // find lesser and greater for active votes
+          const lesserAndGreaterAfterActiveRevoke =
+            await electionWrapper.findLesserAndGreaterAfterVote(
+              group,
+              // @ts-ignore: BigNumber types library conflict.
+              remainingRevokeAmount.mul(-1).toString()
+            );
+          lesserAfterActiveRevoke = lesserAndGreaterAfterActiveRevoke.lesser;
+          greaterAfterActiveRevoke = lesserAndGreaterAfterActiveRevoke.greater;
         }
 
-        // const revokeAmount = scheduledWithdrawalAmount - immediateWithdrawalAmount;
-        console.log("remainingRevokeAmount:", remainingRevokeAmount);
-
-        // get AccountContract pending votes for group.
-        const groupVote = await electionWrapper.getVotesForGroupByAccount(
-          accountContract.address,
-          group
-        );
-        const pendingVotes = groupVote.pending;
-
-        console.log("pendingVotes:", pendingVotes);
-
-        // amount to revoke from pending
-        const toRevokeFromPending: BigNumber = BigNumber.from(
-          Math.min(remainingRevokeAmount.toNumber(), pendingVotes.toNumber())
-        );
-
-        console.log("toRevokeFromPending:", toRevokeFromPending);
-
-        // find lesser and greater for pending votes
-
-        const lesserAndGreaterAfterPendingRevoke =
-          await electionWrapper.findLesserAndGreaterAfterVote(
-            group,
-            // @ts-ignore: BigNumber types library conflict.
-            toRevokeFromPending.mul(-1).toString()
-          );
-        const lesserAfterPendingRevoke = lesserAndGreaterAfterPendingRevoke.lesser;
-        const greaterAfterPendingRevoke = lesserAndGreaterAfterPendingRevoke.greater;
-
-        // Given that validators are sorted by total votes and that revoking pending votes happen before active votes.
-        // One must acccount for any pending votes that would get removed from the total votes when revoking active votes
-        // in the same transaction.
-        const toRevokeFromActive = remainingRevokeAmount;
-
-        console.log("toRevokeFromActive:", toRevokeFromActive);
-
-        // find lesser and greater for active votes
-        const lesserAndGreaterAfterActiveRevoke =
-          await electionWrapper.findLesserAndGreaterAfterVote(
-            group,
-            // @ts-ignore: BigNumber types library conflict.
-            toRevokeFromActive.mul(-1).toString()
-          );
-        const lesserAfterActiveRevoke = lesserAndGreaterAfterActiveRevoke.lesser;
-        const greaterAfterActiveRevoke = lesserAndGreaterAfterActiveRevoke.greater;
-
-        // // find index of group
+        // find index of group
         const index = await findAddressIndex(electionWrapper, group, accountContract.address);
 
         // use current index
