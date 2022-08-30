810afa8096582b2d9453af9a95bc3eece4ef96ff
pahor167 <pavel.hornak@clabs.co>
2022-08-30 11:47:26 +0200

    build fix

--- deploy/00_multisig.ts
+++ deploy/00_multisig.ts
@@ -1,6 +1,5 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { DAY, MINUTE } from "../test-ts/utils";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy } = hre.deployments;
