2b22c56c1f0396aa187f5b0fa78a33320fc7184f
pahor167 <pavel.hornak@clabs.co>
2022-09-12 20:24:59 +0200

    working with proposal

--- test-ts/flows/end-to-end.test.ts
+++ test-ts/flows/end-to-end.test.ts
@@ -8,7 +8,6 @@ import { parseUnits } from "ethers/lib/utils";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 import {
-  ADDRESS_ZERO,
   LOCKED_GOLD_UNLOCKING_PERIOD,
   randomSigner,
   registerValidator,
@@ -17,11 +16,13 @@ import {
   timeTravel,
 } from "../utils";
 import { Manager } from "../../typechain-types/Manager";
-import { MockStakedCelo } from "../../typechain-types/MockStakedCelo";
-import { MockStakedCelo__factory } from "../../typechain-types/factories/MockStakedCelo__factory";
-import { ACCOUNT_ACTIVATE_AND_VOTE, ACCOUNT_WITHDRAW } from "../../lib/tasksNames";
+import {
+  ACCOUNT_ACTIVATE_AND_VOTE,
+  ACCOUNT_WITHDRAW,
+  MULTISIG_EXECUTE_PROPOSAL,
+  MULTISIG_SUBMIT_PROPOSAL,
+} from "../../lib/tasksNames";
 import { StakedCelo } from "../../typechain-types/StakedCelo";
-import { StakedCelo__factory } from "../../typechain-types/factories/StakedCelo__factory";
 import { MultiSig } from "../../typechain-types/MultiSig";
 
 after(() => {
@@ -79,65 +80,47 @@ describe("e2e", () => {
     accountsInstance = await hre.kit.contracts.getAccounts();
     lockedGold = await hre.kit.contracts.getLockedGold();
     election = await hre.kit.contracts.getElection();
-
-    // const stakedCeloFactory: StakedCelo__factory = (
-    //   await hre.ethers.getContractFactory("StakedCelo")
-    // ).connect(owner) as StakedCelo__factory;
-    // stakedCelo = await stakedCeloFactory.deploy(
-
-    // );
-
-    // await hre.deployments.fixture("StakedCelo");
   });
 
   beforeEach(async () => {
     await hre.deployments.fixture("core");
-    const owner = await hre.ethers.getNamedSigner("owner");
-    const deployer = await hre.ethers.getNamedSigner("deployer");
     account = await hre.ethers.getContract("Account");
     managerContract = await hre.ethers.getContract("Manager");
     managerContract = managerContract.attach(await account.manager());
     multisigContract = await hre.ethers.getContract("MultiSig");
-    console.log("Hello");
-    console.log("Account owner", await account.owner());
-    console.log("Manager owner", await managerContract.owner());
-    console.log("myOwner", owner.address);
-    console.log("deployer", deployer.address);
-    console.log("multisigContract", multisigContract.address);
-    // await account.connect(deployer).setManager(managerContract.address);
-
-    const msOwners = await multisigContract.getOwners();
-    console.log("msOwners", msOwners);
 
     stakedCelo = await hre.ethers.getContract("StakedCelo");
-
-    // await stakedCelo.connect(owner).initialize(managerContract.address, owner.address)
-    const stakedCeloOwner = await stakedCelo.owner();
-    console.log("stakedCeloOwner", stakedCeloOwner);
-
-    const myOwner = owner.address;
-    console.log("myOwner", myOwner);
-    // await stakedCelo.connect(owner).setManager(managerContract.address)
   });
 
   it("deposit and withdraw", async () => {
     managerContract.setDependencies(stakedCelo.address, account.address);
 
     const multisigOwner0 = await hre.ethers.getNamedSigner("multisigOwner0");
-    console.log("multisigOwner0", multisigOwner0);
-    // const multisigOwner0 = await hre.ethers.getSigner(multisigContract.address)
 
-    await impersonateAccount(multisigContract.address);
-    const tx = await owner.sendTransaction({
-      to: multisigContract.address,
-      value: ethers.BigNumber.from("10000000000000000000"),
-    });
-    await tx.wait();
-    const signerWithAddress = await ethers.getSigner(multisigContract.address);
+    const payloads: string[] = [];
+    const destinations: string[] = [];
+    const values: string[] = [];
 
     for (let i = 0; i < 3; i++) {
-      await managerContract.connect(signerWithAddress).activateGroup(groupAddresses[i]);
+      destinations.push(managerContract.address);
+      values.push("0");
+      payloads.push(
+        managerContract.interface.encodeFunctionData("activateGroup", [groupAddresses[i]])
+      );
     }
+
+    await hre.run(MULTISIG_SUBMIT_PROPOSAL, {
+      destinations: destinations.join(","),
+      values: values.join(","),
+      payloads: payloads.join(","),
+      account: multisigOwner0.address,
+    });
+
+    await hre.run(MULTISIG_EXECUTE_PROPOSAL, {
+      proposalId: 0,
+      account: multisigOwner0.address,
+    });
+
     await managerContract.connect(depositor).deposit({ value: 100 });
     let stCelo = await stakedCelo.balanceOf(depositor.address);
     expect(stCelo).to.eq(100);
@@ -161,8 +144,4 @@ describe("e2e", () => {
     const depositorAfterWithdrawalBalance = await depositor.getBalance();
     expect(depositorAfterWithdrawalBalance.gt(depositorBeforeWithdrawalBalance)).to.be.true;
   });
-
-  async function impersonateAccount(address: string) {
-    await hre.network.provider.send("hardhat_impersonateAccount", [address]);
-  }
 });
