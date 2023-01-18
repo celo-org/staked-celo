ffff45bc2b315904e32dcdd4c33dcf2ea269afb3
pahor167 <47992132+pahor167@users.noreply.github.com>
2023-01-18 16:13:45 +0100

    All tests working

--- test-ts/address-sorted-linked-list.test.ts
+++ test-ts/address-sorted-linked-list.test.ts
@@ -1,5 +1,7 @@
 import { NULL_ADDRESS } from "@celo/contractkit";
+import BigNumberJs from "bignumber.js";
 import { expect } from "chai";
+import { BigNumber } from "ethers";
 import hre from "hardhat";
 import { MockAddressSortedLinkedList__factory } from "../typechain-types/factories/MockAddressSortedLinkedList__factory";
 import { MockAddressSortedLinkedList } from "../typechain-types/MockAddressSortedLinkedList";
@@ -170,4 +172,209 @@ describe("AddressSortedLinkedList", () => {
       await expect(addressSortedLinkedList.remove(accounts[8])).revertedWith("");
     });
   });
+
+  describe("when there are multiple inserts, updates, and removals", () => {
+    interface SortedElement {
+      key: string;
+      numerator: BigNumber;
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    enum SortedListActionType {
+      // eslint-disable-next-line no-unused-vars
+      Update = 1,
+      // eslint-disable-next-line no-unused-vars
+      Remove,
+      // eslint-disable-next-line no-unused-vars
+      Insert,
+    }
+
+    interface SortedListAction {
+      actionType: SortedListActionType;
+      element: SortedElement;
+    }
+
+    const randomElement = <A>(list: A[]): A => {
+      return list[Math.floor(BigNumberJs.random().times(list.length).toNumber())];
+    };
+
+    const randomElementOrNullAddress = (list: string[]): string => {
+      if (BigNumberJs.random().isLessThan(0.5)) {
+        return NULL_ADDRESS;
+      } else {
+        return randomElement(list);
+      }
+    };
+
+    const makeActionSequence = (length: number, numKeys: number): SortedListAction[] => {
+      const sequence: SortedListAction[] = [];
+      const listKeys: Set<string> = new Set([]);
+      // @ts-ignore
+      const keyOptions = Array.from({ length: numKeys }, () =>
+        hre.web3.utils.randomHex(20).toLowerCase()
+      );
+      for (let i = 0; i < length; i++) {
+        const key = randomElement(keyOptions);
+        let action: SortedListActionType;
+        if (listKeys.has(key)) {
+          action = randomElement([SortedListActionType.Update, SortedListActionType.Remove]);
+          if (action === SortedListActionType.Remove) {
+            listKeys.delete(key);
+          }
+        } else {
+          action = SortedListActionType.Insert;
+          listKeys.add(key);
+        }
+        sequence.push({
+          actionType: action,
+          element: {
+            key,
+            numerator: BigNumber.from(BigNumberJs.random(20).shiftedBy(20).toString()),
+          },
+        });
+      }
+      return sequence;
+    };
+
+    const parseElements = (keys: string[], numerators: BigNumber[]): SortedElement[] =>
+      keys.map((key, i) => ({
+        key: key.toLowerCase(),
+        numerator: numerators[i],
+      }));
+
+    const assertSorted = (elements: SortedElement[]) => {
+      for (let i = 0; i < elements.length; i++) {
+        if (i > 0) {
+          expect(elements[i].numerator.lte(elements[i - 1].numerator)).eq(
+            true,
+            "Elements not sorted"
+          );
+        }
+      }
+    };
+
+    const assertSortedFractionListInvariants = async (
+      elementsPromise: Promise<[string[], BigNumber[]]>,
+      numElementsPromise: Promise<BigNumber>,
+      // medianPromise: Promise<string>,
+      expectedKeys: Set<string>
+    ) => {
+      const [keys, numerators] = await elementsPromise;
+      const elements = parseElements(keys, numerators);
+      expect((await numElementsPromise).toNumber()).to.eq(
+        expectedKeys.size,
+        "Incorrect number of elements"
+      );
+
+      expect(elements.map((x) => x.key).sort()).to.deep.eq(
+        Array.from(expectedKeys.values()).sort(),
+        "keys do not match"
+      );
+
+      assertSorted(elements);
+    };
+
+    const doActionsAndAssertInvariants = async (
+      numActions: number,
+      numKeys: number,
+      // eslint-disable-next-line no-unused-vars
+      getLesserAndGreater: (element: SortedElement) => Promise<{ lesser: string; greater: string }>,
+      allowFailingTx = false
+    ) => {
+      const sequence = makeActionSequence(numActions, numKeys);
+      const listKeys: Set<string> = new Set([]);
+      let successes = 0;
+      for (let i = 0; i < numActions; i++) {
+        const action = sequence[i];
+        try {
+          if (action.actionType === SortedListActionType.Remove) {
+            await addressSortedLinkedList.remove(action.element.key);
+            listKeys.delete(action.element.key);
+          } else {
+            const { lesser, greater } = await getLesserAndGreater(action.element);
+            if (action.actionType === SortedListActionType.Insert) {
+              await addressSortedLinkedList.insert(
+                action.element.key,
+                action.element.numerator,
+                lesser,
+                greater
+              );
+              listKeys.add(action.element.key);
+            } else if (action.actionType === SortedListActionType.Update) {
+              await addressSortedLinkedList.update(
+                action.element.key,
+                action.element.numerator,
+                lesser,
+                greater
+              );
+            }
+          }
+          successes += 1;
+        } catch (e) {
+          if (!allowFailingTx) {
+            throw e;
+          }
+        }
+        await assertSortedFractionListInvariants(
+          addressSortedLinkedList.getElements(),
+          addressSortedLinkedList.getNumElements(),
+          listKeys
+        );
+      }
+      if (allowFailingTx) {
+        const expectedSuccessRate = 2.0 / numKeys;
+        expect(successes / numActions).gte(expectedSuccessRate * 0.75);
+      }
+    };
+
+    it("should maintain invariants when lesser, greater are correct", async () => {
+      const numActions = 100;
+      const numKeys = 20;
+      const getLesserAndGreater = async (element: SortedElement) => {
+        const [keys, numerators] = await addressSortedLinkedList.getElements();
+        const elements = parseElements(keys, numerators);
+        let lesser = NULL_ADDRESS;
+        let greater = NULL_ADDRESS;
+        const value = element.numerator;
+        // Iterate from each end of the list towards the other end, saving the key with the
+        // smallest value >= `value` and the key with the largest value <= `value`.
+        for (let i = 0; i < elements.length; i++) {
+          if (elements[i].key !== element.key.toLowerCase()) {
+            if (elements[i].numerator.gte(value)) {
+              greater = elements[i].key;
+            }
+          }
+          const j = elements.length - i - 1;
+
+          if (elements[j].key !== element.key.toLowerCase()) {
+            if (elements[j].numerator.lte(value)) {
+              lesser = elements[j].key;
+            }
+          }
+        }
+        return { lesser, greater };
+      };
+      await doActionsAndAssertInvariants(numActions, numKeys, getLesserAndGreater);
+    });
+
+    it("should maintain invariants when lesser, greater are incorrect", async () => {
+      const numReports = 200;
+      const numKeys = 20;
+      const getRandomKeys = async () => {
+        let lesser = NULL_ADDRESS;
+        let greater = NULL_ADDRESS;
+        const [keys, , ,] = await addressSortedLinkedList.getElements();
+        if (keys.length > 0) {
+          lesser = randomElementOrNullAddress(keys);
+          greater = randomElementOrNullAddress(keys);
+        }
+        return { lesser, greater };
+      };
+      await doActionsAndAssertInvariants(numReports, numKeys, getRandomKeys, true);
+    });
+  });
 });
+
+export const assertSameAddress = (value: string, expected: string, msg?: string) => {
+  expect(expected.toLowerCase()).to.eq(value.toLowerCase(), msg);
+};
