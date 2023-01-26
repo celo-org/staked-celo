9efb37642d77b191a3cbeafeddc30e537b1f65ea
soloseng <102702451+soloseng@users.noreply.github.com>
2023-01-26 16:38:04 -0500

    -- unused task args

--- lib/multiSig-tasks/encodeProposalSetDependencies.ts
+++ lib/multiSig-tasks/encodeProposalSetDependencies.ts
@@ -1,14 +1,6 @@
-import { task, types } from "hardhat/config";
-import { setLocalNodeDeploymentPath, TransactionArguments } from "../helpers/interfaceHelper";
-import {
-  ACCOUNT,
-  ACCOUNT_DESCRIPTION,
-  MULTISIG_ENCODE_SET_MANAGER_DEPENDENCIES_DESCRIPTION,
-  USE_LEDGER,
-  USE_LEDGER_DESCRIPTION,
-  USE_NODE_ACCOUNT,
-  USE_NODE_ACCOUNT_DESCRIPTION,
-} from "../helpers/staticVariables";
+import { task } from "hardhat/config";
+import { setLocalNodeDeploymentPath } from "../helpers/interfaceHelper";
+import { MULTISIG_ENCODE_SET_MANAGER_DEPENDENCIES_DESCRIPTION } from "../helpers/staticVariables";
 import { taskLogger } from "../logger";
 import {
   MULTISIG_ENCODE_PROPOSAL_PAYLOAD,
@@ -16,31 +8,30 @@ import {
   MULTISIG_SUBMIT_PROPOSAL,
 } from "../tasksNames";
 
-task(MULTISIG_ENCODE_SET_MANAGER_DEPENDENCIES, MULTISIG_ENCODE_SET_MANAGER_DEPENDENCIES_DESCRIPTION)
-  .addOptionalParam(ACCOUNT, ACCOUNT_DESCRIPTION, undefined, types.string)
-  .addFlag(USE_LEDGER, USE_LEDGER_DESCRIPTION)
-  .addFlag(USE_NODE_ACCOUNT, USE_NODE_ACCOUNT_DESCRIPTION)
-  .setAction(async (args: TransactionArguments, hre) => {
-    try {
-      taskLogger.setLogLevel("info");
-      taskLogger.info(`${MULTISIG_ENCODE_SET_MANAGER_DEPENDENCIES} task...`);
-      await setLocalNodeDeploymentPath(hre);
-      const payload = await hre.run(MULTISIG_ENCODE_PROPOSAL_PAYLOAD, {
-        contract: "Manager",
-        function: "setDependencies",
-        args: `${(await hre.deployments.get("StakedCelo")).address},${
-          (
-            await hre.deployments.get("Account")
-          ).address
-        },${(await hre.deployments.get("Vote")).address}`,
-      });
-      const managerAddress = (await hre.deployments.get("Manager")).address;
-      taskLogger.info("--destinations", managerAddress);
-      taskLogger.info("--values", "0");
-      taskLogger.info("--payloads", payload);
+task(
+  MULTISIG_ENCODE_SET_MANAGER_DEPENDENCIES,
+  MULTISIG_ENCODE_SET_MANAGER_DEPENDENCIES_DESCRIPTION
+).setAction(async (_, hre) => {
+  try {
+    taskLogger.setLogLevel("info");
+    taskLogger.info(`${MULTISIG_ENCODE_SET_MANAGER_DEPENDENCIES} task...`);
+    await setLocalNodeDeploymentPath(hre);
+    const payload = await hre.run(MULTISIG_ENCODE_PROPOSAL_PAYLOAD, {
+      contract: "Manager",
+      function: "setDependencies",
+      args: `${(await hre.deployments.get("StakedCelo")).address},${
+        (
+          await hre.deployments.get("Account")
+        ).address
+      },${(await hre.deployments.get("Vote")).address}`,
+    });
+    const managerAddress = (await hre.deployments.get("Manager")).address;
+    taskLogger.info("--destinations", managerAddress);
+    taskLogger.info("--values", "0");
+    taskLogger.info("--payloads", payload);
 
-      taskLogger.info(`Use these values with ${MULTISIG_SUBMIT_PROPOSAL} task`);
-    } catch (error) {
-      taskLogger.error("Error encoding manager setDependencies payload:", error);
-    }
-  });
+    taskLogger.info(`Use these values with ${MULTISIG_SUBMIT_PROPOSAL} task`);
+  } catch (error) {
+    taskLogger.error("Error encoding manager setDependencies payload:", error);
+  }
+});
