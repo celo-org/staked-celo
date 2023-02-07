bbb8b83fdbda4a4cdb6ceebaea5fc7493e8c6ca5
soloseng <102702451+soloseng@users.noreply.github.com>
2023-02-07 13:22:58 -0500

    Soloseng/add-local-node-compatibility-to-view-task (#108)
    * set deployment path when using local node
    
    * using task logger
    
    * ∆ error message
    
    * -- unused task args

--- lib/multiSig-tasks/get-confirmations.ts
+++ lib/multiSig-tasks/get-confirmations.ts
@@ -1,5 +1,5 @@
 import { task, types } from "hardhat/config";
-import { TransactionArguments } from "../helpers/interfaceHelper";
+import { setLocalNodeDeploymentPath, TransactionArguments } from "../helpers/interfaceHelper";
 import {
   LOG_LEVEL,
   LOG_LEVEL_DESCRIPTION,
@@ -16,6 +16,7 @@ task(MULTISIG_GET_CONFIRMATIONS, MULTISIG_GET_CONFIRMATIONS_TASK_DESCRIPTION)
   .setAction(async (args: TransactionArguments, hre) => {
     taskLogger.setLogLevel(args.logLevel);
     try {
+      await setLocalNodeDeploymentPath(hre);
       const multiSigContract = await hre.ethers.getContract("MultiSig");
       const confirmations = await multiSigContract.getConfirmations(args.proposalId);
       taskLogger.log("Addresses that have confirmed the proposal:", confirmations);
