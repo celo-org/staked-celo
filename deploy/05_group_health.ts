90684410edb4825778cbfe32818a00120aa9673e
pahor167 <47992132+pahor167@users.noreply.github.com>
2023-02-13 14:09:14 +0100

    Update group health deploy script

--- deploy/05_group_health.ts
+++ deploy/05_group_health.ts
@@ -2,11 +2,16 @@ import { DeployFunction } from "@celo/staked-celo-hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { catchNotOwnerForProxy } from "../lib/deploy-utils";
 
+const parseValidatorGroups = (validatorGroupsString: string | undefined) =>
+  validatorGroupsString ? validatorGroupsString.split(",") : [];
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy } = hre.deployments;
   const { deployer } = await hre.getNamedAccounts();
 
   const multisig = await hre.deployments.get("MultiSig");
+
+  const isGroupHealthAlreadyDeployed = await hre.deployments.getOrNull("GroupHealth");
 
   await catchNotOwnerForProxy(
     deploy("GroupHealth", {
@@ -24,6 +29,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       },
     })
   );
+
+  if (!isGroupHealthAlreadyDeployed) {
+    const validatorGroups = parseValidatorGroups(process.env.VALIDATOR_GROUPS);
+    if (validatorGroups.length == 0) {
+      return;
+    }
+
+    const groupHealth = await hre.ethers.getContract("GroupHealth");
+
+    for (let i = 0; i < validatorGroups.length; i++) {
+      const updateGroupHealthTx = await groupHealth.updateGroupHealth(validatorGroups[i]);
+      await updateGroupHealthTx.wait();
+    }
+  }
 };
 
 func.id = "deploy_group_health";
